a3469cc807209a0c548d253326b020846f5f6874
Mark Wong <markmcwong@yahoo.com>
2021-05-17 14:40:16 +0800

    Attempt to fix package import

--- test/myHomeScreen_test.dart
+++ test/myHomeScreen_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:TimeliNUS/screens/myHomePage.dart';
+import 'package:TimeliNUS/screens/myHomeScreen.dart';
 
 void main() {
   testWidgets('home page is created', (WidgetTester tester) async {
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:TimeliNUS/screens/myHomePage.dart';
+import 'package:TimeliNUS/screens/myHomeScreen.dart';
 import 'package:TimeliNUS/main.dart';
 
 void main() {
